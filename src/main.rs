63ae462e6d392f2110f8409b6d97f0fa5201f43c
Justin Liew <justinliew@gmail.com>
2019-03-27 23:38:28 -0700

    mock tun-tap so I can developer higher level on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+//extern crate tun_tap;
+
 use std::io;
+
+mod tun_tap;
 
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
-    let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x}", nbytes, buf[..nbytes]);
+    // let mut buf = [0u8; 1504];
+    // let nbytes = nic.recv(&mut buf[..])?;
+    // eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
     Ok(())
 }
---
+++ src/tun_tap.rs
@@ -0,0 +1,14 @@
+pub enum Mode {
+    Tun,
+    Tap,
+}
+
+pub struct Iface;
+
+impl Iface {
+
+    // gotta fix this
+    fn new(name: &str, mode: Mode) -> Result<Self> {
+    }
+
+}
