4424abf52435d517d0317528e0b93af51ef3bf58
Justin Liew <justinliew@gmail.com>
2019-04-25 07:01:12 -0700

    fixing compile errors

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,17 +1,22 @@
-use io;
+use std::io;
 
-#[derive(Debug,Clone,Copy,Hash,Eq,PartialEq)]
-pub enum State {
+enum State {
     Closed,
     Listen,
-    SynRcvd,
-    Estab,
+    // SynRcvd,
+    // Estab,
 }
 
-impl Default for State {
+pub struct Connection {
+    state: State
+}
+
+impl Default for Connection {
     fn default() -> Self {
         // State::Closed // temporary for easy testing
-        State::Listen
+        Connection {
+            state: State::Listen,
+        }
     }
 }
 
@@ -31,31 +36,46 @@ impl State {
         match *self {
             State::Closed => {
                 eprintln!("We are in closed state; received unexpected packet");
-                return;
+                return Ok(0);
             },
             State::Listen => {
                 if !tcph.syn() {
                     // only expected syn packet; got sometihng else
                     eprintln!("We are in Listen state; received unexpected non-syn packet");
-                    return;
+                    return Ok(0);
                 }
 
                 // establish a connection
-                let mut syn_ack = etherparse::TcpHeader::new(tcph.destination_port(), tcph.source_port(),unimplemented!(), unimplemented!());
+                let mut syn_ack = etherparse::TcpHeader::new(tcph.destination_port(), 
+                                                            tcph.source_port(),
+                                                            unimplemented!(), 
+                                                            unimplemented!());
                 syn_ack.syn = true;
                 syn_ack.ack = true;
-                let mut ip = etherparse::Ipv4Header::new(syn_ack.slice().len(), 64, etherparse::IpTrafficClass::Tcp, iph.destination_addr(), iph.source_addr());
+                let mut ip = etherparse::Ipv4Header::new(syn_ack.header_len(), 
+                                                            64, 
+                                                            etherparse::IpTrafficClass::Tcp, 
+                                                            [
+                                                                iph.destination()[0],
+                                                                iph.destination()[1],
+                                                                iph.destination()[2],
+                                                                iph.destination()[3],
+                                                            ],
+                                                            [
+                                                                iph.source()[0],
+                                                                iph.source()[1],
+                                                                iph.source()[2],
+                                                                iph.source()[3],
+                                                            ]);
 
                 let unwritten = {
                     let mut unwritten = &mut buf[..];
-                    ip.write(unwritten);
-                    syn_ack.write(unwritten);
+                    ip.write(&mut unwritten);
+                    syn_ack.write(&mut unwritten);
                     unwritten.len()
                 };
-                nic.send(&buf[..unwritten])?
+                nic.send(&buf[..unwritten])
             },
-            State::SynRcvd => {},
-            State::Estab => {},
         }
     }
 }
